7a8c2509ae9b977ac0f422c11028b929508fe4ad
Edgar Lipnevic <edgarlipnevic@gmail.com>
2025-01-07 21:43:38 +0200

    copy function add

--- script.js
+++ script.js
@@ -1,7 +1,9 @@
+//1.
 const Generatebutton = document.querySelector("generatePasswordBtn");
 const Copybutton = document.querySelector("copyPasswordBtn");
 Generatebutton.addEventListener("click", generatePassword);
-
+Copybutton.addEventListener("click", copypaste);
+//2.
 function generatePassword() {
   const lenght = document.querySelector("passwordLenght").value;
   const uppercase = document.querySelector("includeUppercase").checked;
@@ -12,4 +14,21 @@ function generatePassword() {
   const uppercaseLetters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
   const numbers = "0123456789";
   const symbols = "!@#$%^&*()_+[]{}|;:,.<>?";
+}
+
+//last
+function copypaste() {
+  const passwordText = document.getElementById("passwordOutput").innerHTML;
+  if (passwordText) {
+    navigator.clipboard
+      .writeText(passwordText)
+      .then(() => {
+        alert("Password copied to clipboard");
+      })
+      .catch(() => {
+        alert("Failed to copy password");
+      });
+  } else {
+    alert("No password to copy");
+  }
 }
